b482d37d4d07ac669b8b0b14659204d7d963a865
AlexandreCrevel <94857674+AlexandreCrevel@users.noreply.github.com>
2024-10-30 10:45:37 +0100

    ✨ Welcome screen OK

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,58 @@
+import SimpleButton from '@/components/SimpleButton';
+import { useAssets } from 'expo-asset';
+import { ResizeMode, Video } from 'expo-av';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const Page = () => {
+  const [assets] = useAssets([require('@/assets/videos/intro.mp4')]);
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={styles.container}>
+      {assets && (
+        <Video
+          resizeMode={ResizeMode.COVER}
+          isMuted
+          isLooping
+          shouldPlay
+          source={{ uri: assets[0].uri }}
+          style={styles.video}
+        />
+      )}
+      <View style={{ marginTop: 80, padding: 20 }}>
+        <Text style={styles.header}>Ready to change the way you money?</Text>
+      </View>
+      <View style={styles.buttons}>
+        <SimpleButton href='/login' text='Login In' variant='dark' />
+        <SimpleButton href='/signup' text='Sign Up' variant='light' />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  video: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+  },
+  header: {
+    fontSize: 36,
+    textTransform: 'uppercase',
+    color: 'white',
+    fontWeight: '900',
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 20,
+    marginBottom: 60,
+    color: 'red',
+    paddingHorizontal: 20,
+  },
+});
 
 export default Page;
